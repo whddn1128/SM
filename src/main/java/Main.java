f8f066eb5317c754400ba7200e225d9e280d1350
HanSM <HanSM@HanSM-PC>
2018-05-11 21:44:07 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world2!");
+		System.out.println("Hello world2345!");
+		System.out.println("어케 해야되죵");
+		
 
 	}
 
