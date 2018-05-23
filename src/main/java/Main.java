009f9d4e5c1a0d4769c1eeef4753e69e2c0643ca
HanSM <HanSM@HanSM-PC>
2018-05-23 15:34:29 +0900

    초기본 입니다

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world2345!");
-		System.out.println("어케 해야되죵");
-		
+	Controller c=new Controller();
+	String category;
+	
+	c.get_Account();
+	System.out.println("type in the menu=> send withdraw deposit check remain");
+	Scanner s=new Scanner(System.in);
+	category=s.nextLine();
+	c.category(category);
+	s.close();
 
 	}
 
 }
+ 
